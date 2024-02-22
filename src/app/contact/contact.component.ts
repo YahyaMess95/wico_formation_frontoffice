192d101132e187e38c2e256edc8181b87349058e
ThemerGfx <thamer.gouider@gmail.com>
2024-02-22 12:47:52 +0100

    fix contact page

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,45 +1,67 @@
 import { Component, OnInit } from '@angular/core';
-import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { NavigationEnd, Router } from '@angular/router';
 import emailjs, { EmailJSResponseStatus } from '@emailjs/browser';
 import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.css'
+  styleUrl: './contact.component.css',
 })
 export class ContactComponent implements OnInit {
   submitted = false;
-  constructor(private toastr: ToastrService,private formBuilder: FormBuilder) {}
-  
-  Forme:FormGroup = new FormGroup({
+  showFixedNavBar: boolean = false;
+  constructor(
+    private toastr: ToastrService,
+    private formBuilder: FormBuilder,
+    private router: Router
+  ) {}
+
+  Forme: FormGroup = new FormGroup({
     name: new FormControl(''),
     email: new FormControl(''),
     message: new FormControl(''),
   });
-  
+
   ngOnInit(): void {
-   this.Forme = this.formBuilder.group(
-      {
-        name: ['', Validators.required, Validators.minLength(3)],
-        email: ['', [Validators.required, Validators.email]],
-        message: ['', Validators.required, Validators.minLength(3)],
+    this.Forme = this.formBuilder.group({
+      name: ['', Validators.required, Validators.minLength(3)],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', Validators.required, Validators.minLength(3)],
+    });
+
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.showFixedNavBar = this.shouldShowFixedNavBar(event.url);
       }
-    );
+    });
+  }
+
+  shouldShowFixedNavBar(url: string): boolean {
+    return url.includes('/contact');
   }
   get f(): { [key: string]: AbstractControl } {
     return this.Forme.controls;
   }
 
-  showSuccess(title:string,subtitle:string) {
-    this.toastr.success(title,subtitle), {
-      timeOut: 3000,
-    };
+  showSuccess(title: string, subtitle: string) {
+    this.toastr.success(title, subtitle),
+      {
+        timeOut: 3000,
+      };
   }
-  showError(title:string,subtitle:string) {
-    this.toastr.error(title,subtitle), {
-      timeOut: 3000,
-    };
+  showError(title: string, subtitle: string) {
+    this.toastr.error(title, subtitle),
+      {
+        timeOut: 3000,
+      };
   }
 
   public sendEmail(e: Event) {
@@ -49,17 +71,23 @@ export class ContactComponent implements OnInit {
       return;
     }
     e.preventDefault();
-    emailjs.sendForm('service_6y3o10a', 'template_qu7gmif', e.target as HTMLFormElement, 'UOnDJ0Tt7O1iwHU1B')
-      .then((result: EmailJSResponseStatus) => {
-        console.log(result.text);
-        this.showSuccess('Email Sended','Successfully!');
-        this.Forme.reset();
-      }, (error) => {
-        console.log(error.text);
-        this.showError(error.text,'Erreur!');
-      });
+    emailjs
+      .sendForm(
+        'service_6y3o10a',
+        'template_qu7gmif',
+        e.target as HTMLFormElement,
+        'UOnDJ0Tt7O1iwHU1B'
+      )
+      .then(
+        (result: EmailJSResponseStatus) => {
+          console.log(result.text);
+          this.showSuccess('Email Sended', 'Successfully!');
+          this.Forme.reset();
+        },
+        (error) => {
+          console.log(error.text);
+          this.showError(error.text, 'Erreur!');
+        }
+      );
   }
-
-
-
 }
