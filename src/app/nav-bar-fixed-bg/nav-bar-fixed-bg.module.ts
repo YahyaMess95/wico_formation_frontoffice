af2ad99358a412221636cd338a1186831ba8a98f
ThemerGfx <thamer.gouider@gmail.com>
2024-03-02 18:58:06 +0100

    fix dark theme

--- src/app/nav-bar-fixed-bg/nav-bar-fixed-bg.module.ts
+++ src/app/nav-bar-fixed-bg/nav-bar-fixed-bg.module.ts
@@ -1,11 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NavBarFixedBgComponent } from './nav-bar-fixed-bg.component';
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: [NavBarFixedBgComponent]
+  imports: [CommonModule],
+  declarations: [],
 })
-export class NavBarFixedBgModule { }
+export class NavBarFixedBgModule {}
